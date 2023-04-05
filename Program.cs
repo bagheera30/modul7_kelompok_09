389f0c44f85fb890660cd5f1bd0145149a16b9c4
david <ryansiahaan@student.telkomuniversity.ac.id>
2023-04-05 16:46:09 +0700

    menambahkan class DataMahasiswa1302200003

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("test");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using modul7_kelompok_09;
+
+class main
+{
+    public static void Main()
+    {
+        DataMahasiswa1302200003 mhsA1 = new DataMahasiswa1302200003();
+        mhsA1.ReadJSON();
+    }
+}
